0fc69e741dcea8bc48e16c424ab1706ad14fc07b
unknown <ksdesportes@gmail.com>
2020-05-07 23:43:47 -0400

    Additional layout updates that will probably be changed

---
+++ js/controller.js
@@ -0,0 +1,12 @@
+var controller = {
+};
+
+controller.initialize = function () {
+    console.log("CONTROLLER: Initializing...");
+    controller.setupListeners();
+};
+
+controller.setupListeners = function() {
+	console.log("CONTROLLER: Setting up listeners..."); 
+
+};
